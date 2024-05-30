16401f1f3e951f6e3251ac84ab2277df3b646f40
Bipin kumar <brgaya9431@gmail.com>
2024-05-30 08:11:05 +0530

    utils code discussion

--- src/index.js
+++ src/index.js
@@ -6,8 +6,15 @@ dotenv.config({
 })
 
 
-connectDB();
-
+connectDB()
+.then(()=>{
+   app.liten(process.env.PORT || 8000,() => {
+      console.log(`server is running at port : ${process.env.PORT}`);
+   })
+})
+.catch((err)=>{
+   console.log("MongDb connection failed !!!",err)
+})
 
 
 
