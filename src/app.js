84f6c3c3f73197d05c53f37ba33880a4205b1b58
Bipin kumar <brgaya9431@gmail.com>
2024-07-02 14:53:47 +0530

    dashBoard section is added

--- src/app.js
+++ src/app.js
@@ -20,6 +20,7 @@ import commentRouter from "./routes/comment.routes.js"
 import videoRouter from "./routes/video.routes.js"
 import likeRouter from "./routes/like.routes.js"
 import playlistRouter from "./routes/playlist.routes.js"
+import dashboardRouter from "./routes/dashboard.routes.js"
 
 
 // routes declaration
@@ -29,6 +30,7 @@ app.use("/api/v1/comments", commentRouter)
 app.use("/api/v1/videos", videoRouter)
 app.use("/api/v1/likes", likeRouter)
 app.use("/api/v1/playlists", playlistRouter)
+app.use("/api/v1/dashboards",dashboardRouter)
 
 
 
